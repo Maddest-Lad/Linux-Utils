e0ddd688e28f144577fd039473d16116b247c530
Sam <44209585+SamHarris2020@users.noreply.github.com>
2022-04-14 18:14:49 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,29 +5,16 @@ mkdir ~/.old-config
 cp ~/.bashrc ~/.old/.bashrc 2>/dev/null
 cp ~/.bash_aliases  ~/.old/.bash_aliases 2>/dev/null
 
-# Use Correct Bashrc File Depending on OS
-distro="$(lsb_release -is)" # "Ubuntu" "Kali"
-
-if [$distro = "Ubuntu"]
-  then cp bashrc-ubuntu ~/.bashrc
-
-  elif [$distro = "Kali"]
-    then cp bashrc-kali ~/.bashrc
-
-  else
-    # The Ubuntu Config is the Most Generic, So It Ought to Work on Most Systems
-    echo "$distro Not Recognised, Override? (Yes/No)? "
-    select yn in "Yes" "No"; do
-      case $yn in
-        Yes ) cp bashrc-ubuntu ~/.bashrc; break;;
-        No ) exit;;
-      esac
-    done
-fi
+echo "Which Distro Are You Installing On? "
+select yn in "Ubuntu" "Kali" "Generic Install" "Quit"; do
+  case $yn in
+    Ubuntu ) cp bashrc-ubuntu ~/.bashrc; break;;
+    Kali ) cp bashrc-kali ~/.bashrc; break;;
+    Generic Install ) cp bashrc-ubuntu ~/.bashrc; break;;
+    Quit ) exit;;
+  esac
+done
 
 # Setup Aliases
 cp bash_aliases ~/.bash_aliases
-
-# Source Everything
 source ~/.bashrc
-source ~/.bash_aliases
