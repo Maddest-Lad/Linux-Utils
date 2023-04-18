147f8ad4f689ebabb4e412c1cfdd6169ab5f8e66
Sam <44209585+Maddest-Lad@users.noreply.github.com>
2023-04-18 08:53:55 -0700

    Update install.sh
    Removed unused flow

--- install.sh
+++ install.sh
@@ -4,20 +4,15 @@
 mkdir ~/.old
 cp ~/.bashrc ~/.old/.bashrc 2>/dev/null
 cp ~/.bash_aliases  ~/.old/.bash_aliases 2>/dev/null
-cd Linux-Utils
 
-echo "Which Distro Are You Installing On? "
-select yn in "Ubuntu" "Kali" "Generic" "Quit"; do
-  case $yn in
-    Ubuntu ) cp bashrc-ubuntu ~/.bashrc; break;;
-    Kali ) cp bashrc-kali ~/.bashrc; break;;
-    Generic ) cp bashrc-ubuntu ~/.bashrc; break;;
-    Quit ) exit;;
-  esac
-done
+# Enter Into Setup Folder
+cd Linux-Utils
+cp bashrc-ubuntu ~/.bashrc
 
 # Setup Aliases
 cp bash_aliases ~/.bash_aliases
+
+# Load new bashrc
 source ~/.bashrc
 
 # Upgrade Nano
@@ -26,3 +21,6 @@ chmod +x upgrade_nano.sh && ./upgrade_nano.sh
 # Setup Base Packages
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y ssh mdadm htop tree net-tools git git-lfs ffmpeg python3 python3-pip virtualenv
+
+# Return to parent directory
+cd ..
