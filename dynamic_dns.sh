187795781826b6f645dc6ed1442a9676383d4645
Sam <44209585+Maddest-Lad@users.noreply.github.com>
2023-04-18 09:53:30 -0700

    Create dynamic_dns.sh

---
+++ dynamic_dns.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Create Working Directory
+mkdir -p /DNS
+
+# Run in the Right Place
+cd /home/$USER/DNS
+
+USERNAME=""
+PASSWORD=""
+HOSTNAME=""
+
+# Load External IP
+source external_ip
+
+# Resolve current public IP
+NEW_IP=$(curl -s "https://domains.google.com/checkip")
+# echo "$IP $NEW_IP"
+
+# Check
+if [ "$IP" != "$NEW_IP" ]; then
+        # Update Google DNS Record
+        URL="https://${USERNAME}:${PASSWORD}@domains.google.com/nic/update?hostname=${HOSTNAME}&myip=${IP}"
+        curl -s $URL
+
+        echo "$(date), Updated $IP to $NEW_IP" >> log.csv
+        IP="$NEW_IP"
+        declare -p IP > external_ip
+
+else
+        echo "No Change Detected"
+fi
